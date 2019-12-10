ae24e55848f27f255b19f81d479f04bfec2a4d43
Harsh Patni <patni.harsh5@gmail.com>
2019-12-10 02:43:16 -0600

    Updates

---
+++ precache-manifest.f5f76932c525fe4fd4233128b840949f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ead690ce43b33bf0fad7b78781773b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2856f9da90716bb468df",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "a71849bd7bb6eb95f65a",
+    "url": "/static/js/2.98813178.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.98813178.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2856f9da90716bb468df",
+    "url": "/static/js/main.9bc31990.chunk.js"
+  },
+  {
+    "revision": "e51e9451d863f9b4bc29",
+    "url": "/static/js/runtime-main.917d8f68.js"
+  }
+]);
